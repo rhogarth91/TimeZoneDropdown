776ad1109eb0f4e84f34674f198671edf21728a0
rhogarth91 <148714164+rhogarth91@users.noreply.github.com>
2024-01-13 18:55:20 +0000

    Function to alert user of selected country and to show no popup if no country selected at all

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 function showSelectedCountry(event) {
-  alert(event.target.value);
+  if (event.target.value.length > 0) {
+    if (event.target.value === "paris") {
+      alert("Paris");
+    }
+    {
+      if (event.target.value === "tokyo") alert("Tokyo");
+    }
+    {
+      if (event.target.value === "sydney") alert("Sydney");
+    }
+  }
 }
 
 let countrySelected = document.querySelector("#countries");
