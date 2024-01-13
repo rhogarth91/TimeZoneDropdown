a3710819717240a784fa09e2376e2552e7008a7a
rhogarth91 <148714164+rhogarth91@users.noreply.github.com>
2024-01-13 19:06:53 +0000

    Function updated to show current live time in selected country

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 function showSelectedCountry(event) {
   if (event.target.value.length > 0) {
     if (event.target.value === "paris") {
-      alert("Paris");
+      alert(`It is ${parisTime}`);
     }
     {
-      if (event.target.value === "tokyo") alert("Tokyo");
+      if (event.target.value === "tokyo") alert(`It is ${tokyoTime}`);
     }
     {
-      if (event.target.value === "sydney") alert("Sydney");
+      if (event.target.value === "sydney") alert(`It is ${sydneyTime}`);
     }
   }
 }
 
 let countrySelected = document.querySelector("#countries");
 countrySelected.addEventListener("change", showSelectedCountry);
+
+let parisTime = moment().tz("Europe/Paris").format("dddd MMMM D, YYYY h:m A");
+let tokyoTime = moment().tz("Asia/Tokyo").format("dddd MMMM D, YYYY h:m A");
+let sydneyTime = moment()
+  .tz("Australia/Sydney")
+  .format("dddd MMMM D, YYYY h:m A");
