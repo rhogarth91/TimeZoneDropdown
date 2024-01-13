0806ced5811a90a183e08820e804f50d6bb94b90
rhogarth91 <148714164+rhogarth91@users.noreply.github.com>
2024-01-13 19:56:55 +0000

    Recoded function
    -Reduces need to have multiple city variables looking at the moment cdn inserted in the HTML head in the file setup stage
    -HTML now looks directly at cdn data and allows more options to the dropdown to be added (with the need to correct endless variables in JS)
    - JS code now looks for its target from HTML code rather than from a variable

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 function showSelectedCountry(event) {
   if (event.target.value.length > 0) {
-    if (event.target.value === "paris") {
-      alert(`It is ${parisTime} in Europe/Paris`);
-    }
-    {
-      if (event.target.value === "tokyo")
-        alert(`It is ${tokyoTime} in Asia/Tokyo`);
-    }
-    {
-      if (event.target.value === "sydney")
-        alert(`It is ${sydneyTime} in Australia/Sydney`);
-    }
+    let countryCurrentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY h:mm A");
+
+    alert(`It is ${countryCurrentTime} in ${event.target.value}`);
   }
 }
 
 let countrySelected = document.querySelector("#countries");
 countrySelected.addEventListener("change", showSelectedCountry);
-
-let parisTime = moment().tz("Europe/Paris").format("dddd MMMM D, YYYY h:m A");
-let tokyoTime = moment().tz("Asia/Tokyo").format("dddd MMMM D, YYYY h:m A");
-let sydneyTime = moment()
-  .tz("Australia/Sydney")
-  .format("dddd MMMM D, YYYY h:m A");
